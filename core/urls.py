8f189bc3459ef29bb824e3caaf23f1c78d690319
ThéoFarhat <theo@ejcm.com.br>
2023-12-16 17:53:49 -0300

    Search bar + tela de cadastro de ativo

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
+    path('assets/', views.assets, name='assets'),
+    path('get-suggestions/', views.get_suggestions, name='get-suggestions'),
 ]
 
--- core/views.py
+++ core/views.py
@@ -1,19 +1,45 @@
 from django.shortcuts import render
 import requests
+from django.http import JsonResponse
 
-def home(request):
-    api_html = api_b3(request).content.decode('utf-8')
-    return render(request, 'core/home.html', {'api_html': api_html})
+def home(request): 
+    return render(request, 'core/home.html')
 
 def login(request):
     return render(request, 'core/login.html')
 
-def api_b3(request):
-    try:
-        response = requests.get('https://brapi.dev/api/quote/PETR4?interval=1d&token=8rDXEbmSajqBqWekoG1rTh')
-        data = response.json()
-    except requests.RequestException as e:
-        print(f"API request error: {e}")
-        data = None
-    return render(request, 'core/api.html', {'data': data})
 
+from django.shortcuts import render
+from django.http import JsonResponse
+import requests
+
+def assets(request):
+    data = None
+
+    if 'query' in request.GET:
+        query = request.GET['query']
+        try:
+            api_url = f'https://brapi.dev/api/quote/list?&token=8rDXEbmSajqBqWekoG1rTh&search={query}'
+            response = requests.get(api_url)
+            data = response.json()
+        except requests.RequestException as e:
+            print(f"API request error: {e}")
+
+    return render(request, 'core/assets.html', {'data': data})
+
+def get_suggestions(request):
+    data = None
+    try:
+        if 'query' in request.GET:
+            query = request.GET['query']
+            api_url = f'https://brapi.dev/api/quote/list?&token=8rDXEbmSajqBqWekoG1rTh'
+            response = requests.get(api_url)
+            data = response.json() 
+            
+            if data and 'stocks' in data:
+                filtered_stocks = [stock for stock in data['stocks'] if query.lower() in stock['name'].lower()]
+                data['stocks'] = filtered_stocks
+    except requests.RequestException as e:
+        print(f"API request error: {e}") 
+
+    return JsonResponse({'data': data})
